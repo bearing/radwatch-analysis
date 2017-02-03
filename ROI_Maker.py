aee83a59f7d9e239e812d79731a9e71e9fdd4099
msaade <msaade@berkeley.edu>
2017-02-03 11:57:17 -0800

    Update ROI_Maker

--- ROI_Maker.py
+++ ROI_Maker.py
@@ -1,14 +1,18 @@
 #from Updated_Peak_Finder import peak_finder_pro as peak_finder
 import numpy as np
 import peakutils
+import isotope_identification
 
-def ROI_Maker(spectrum, energy, sub_regions='both'):
+def ROI_Maker(spectrum, energy,sub_regions='auto'):
     """
-    Make ROIs for one peak.
+    Takes in a measured spectrum and identifies peaks in terms of channel number
+    and energy. Uses peak_finder to localize the peak more precisely from the
+    given energy. Also returns peak and compton ROIs as well as each ROIs gross
+    area.
     """
-    
+
     peak_energy = peak_finder(spectrum, energy)
-    
+
     E0 = spectrum.energy_cal[0]
     Eslope = spectrum.energy_cal[1]
     M_counts = spectrum.data
@@ -28,6 +32,12 @@ def ROI_Maker(spectrum, energy, sub_regions='both'):
 
     right_center = energy_channel + compton_distance * fwhm_channel
     right_ch = (right_center - fwhm_channel, right_center + fwhm_channel)
+
+    if sub_regions == 'auto':
+        if energy == Cs_134_g_e[2]:
+            sub_regions = 'Cs134'
+        else:
+            sub_regions = 'both'
 
     if sub_regions == 'left':
         side_region_list = [left_ch]
@@ -50,13 +60,12 @@ def ROI_Maker(spectrum, energy, sub_regions='both'):
         side_region_list = [left_ch, right_ch]
 
     return peak_ch, side_region_list
-    
-    
-    
+
+
 def peak_finder(spectrum, energy):
     '''
     PEAK_FINDER will search for peaks within a certain range determined by the
-    Energy given. It takes a Spectra file and an Energy value as input. The
+    Energy given. It takes a spectrum object and an Energy value as input. The
     energy range to look in is given by the Full-Width-Half-Maximum (FWHM).
     If more than one peak is found in the given range, the peak with the
     highest amount of counts will be used.
