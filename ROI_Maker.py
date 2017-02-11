f95252a28823cd59fc0170eb307ce588e1ea7a61
msaade <msaade@berkeley.edu>
2017-02-10 16:55:41 -0800

    Cleaned ROI_Maker
    Rewrote all the ROI channel calculations in terms of the region_width and region_separation variables.

--- ROI_Maker.py
+++ ROI_Maker.py
@@ -1,4 +1,3 @@
-#from Updated_Peak_Finder import peak_finder_pro as peak_finder
 import numpy as np
 import peakutils
 from Isotope_identification import Cs_134_g_e
@@ -8,15 +7,13 @@ def ROI_Maker(spectrum, energy,sub_regions='auto'):
     """
     Takes in a measured spectrum and identifies peaks in terms of channel number
     and energy. Uses peak_finder to localize the peak more precisely from the
-    given energy. Also returns peak and compton ROIs as well as each ROIs gross
-    area.
+    given energy. Also returns peak and compton ROIs.
     """
 
     peak_energy = peak_finder(spectrum, energy)
 
     E0 = spectrum.energy_cal[0]
     Eslope = spectrum.energy_cal[1]
-    M_counts = spectrum.data
     energy_channel = int((peak_energy - E0) / Eslope)
 
     region_size = 1.3
@@ -24,15 +21,17 @@ def ROI_Maker(spectrum, energy,sub_regions='auto'):
 
     # Rough estimate of FWHM.
     fwhm = 0.05*peak_energy**0.5
-    fwhm_channel = int(region_size * (fwhm - E0) / Eslope)
-    # peak gross area
-    peak_ch = (energy_channel - fwhm_channel, energy_channel + fwhm_channel)
 
-    left_center = energy_channel - compton_distance * fwhm_channel
-    left_ch = (left_center - fwhm_channel, left_center + fwhm_channel)
+    region_width = int(region_size * (fwhm - E0) / Eslope)
+    region_separation = int(compton_distance*region_width)
 
-    right_center = energy_channel + compton_distance * fwhm_channel
-    right_ch = (right_center - fwhm_channel, right_center + fwhm_channel)
+    peak_ch = (energy_channel - region_width, energy_channel + region_width)
+
+    left_center = energy_channel - region_separation
+    left_ch = (left_center - region_width, left_center + region_width)
+
+    right_center = energy_channel + region_separation
+    right_ch = (right_center - region_width, right_center + region_width)
 
     if sub_regions == 'auto':
         if energy == Cs_134_g_e[2]:
@@ -47,13 +46,11 @@ def ROI_Maker(spectrum, energy,sub_regions='auto'):
     elif sub_regions == 'Cs134':
         # Cs134 compton region using Bi214 609 peak.
         bi_fwhm = 0.05 * (609.31)**0.5
-        bi_fwhm_channel = int(region_size * (bi_fwhm - E0) / Eslope)
+        bi_region_width = int(region_size * (bi_fwhm - E0) / Eslope)
+        bi_region_separation = int(compton_distance*bi_region_width)
         bi_peak_channel = int((609.31 - E0) / Eslope)
-        bi_right_peak = bi_peak_channel + compton_distance * bi_fwhm_channel
-        bi_right_compton = sum(M_counts[(bi_right_peak - fwhm_channel):
-                                        (bi_right_peak + fwhm_channel)])
-
-        bi_right_ch = (bi_right_peak - fwhm_channel, bi_right_peak + fwhm_channel)
+        bi_right_peak = bi_peak_channel + bi_region_separation
+        bi_right_ch = (bi_right_peak - region_width, bi_right_peak + region_width)
         side_region_list = [left_ch, bi_right_ch]
     elif sub_regions == 'none':
         side_region_list = []
