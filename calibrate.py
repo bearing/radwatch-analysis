658180ed8d6d7354ac51da3e1b40bc41f29fd3cf
Yaro Kaminskiy <yaroslavk@berkeley.edu>
2017-03-30 01:18:12 -0700

    Update calibrate.py

--- calibrate.py
+++ calibrate.py
@@ -27,14 +27,18 @@ def acquire_files():
             elif filename == "UCB018_Soil_Sample010_2.Spe":
                 pass
             else:
-                if '_recal.spe' in filename:
+                if '_recal.Spe' in filename:
                     recal_names.append(filename)
                 sample_measurements.append(filename)
 
     sample_measurements.sort()
 
+    print("recal_names: ", recal_names, '\n\n')
+
     for recal_name in recal_names:
         recal_focus = recal_name.replace('_recal', '')
+
+        print(recal_focus)
 
         if recal_focus in sample_measurements:
             sample_measurements.remove(recal_focus)
@@ -42,6 +46,9 @@ def acquire_files():
     sample_names = [os.path.splitext(sample_measurements[i])
                     [0].replace("_", " ")
                     for i in np.arange(len(sample_measurements))]
+
+    print("sample_names: ", sample_names, '\n\n')
+    print("sample_measurements: ", sample_measurements, '\n\n')
 
     return sample_measurements, sample_names
 
@@ -139,7 +146,7 @@ def recalibrate(files):
     cal_offsets = []
 
     for sample in files:
-        if '_recal.spe' in sample.lower():
+        if '_recal.Spe' in sample.lower():
             double_check.append(sample)
             pass
         else:
@@ -166,11 +173,15 @@ def recalibrate(files):
         with open('Error_Cal.txt', 'w') as file:
             file.writelines('Check calibration in %s \n' % error for error in
                             cal_error)
-    for file in files:
-        if '_recal.Spe' in file:
-            files.remove(file)
+    # for file in files:
+    #     if '_recal.Spe' in file:
+    #         files.remove(file)
 
-    calibration_table(files, cal_headers, cal_offsets)
+    files_updated = [os.path.splitext(files[i])
+                     [0].replace("_recal", "")
+                     for i in np.arange(len(files))]
+
+    calibration_table(files_updated, cal_headers, cal_offsets)
 
 
 def calibration_table(samples, headers, offsets):
@@ -184,12 +195,7 @@ def calibration_table(samples, headers, offsets):
     cal_frame = cal_frame.T
     cal_frame.index.name = 'Filename'
     cal_frame.to_csv('calibration_results.csv')
-    
-def get_sample_names(sample_measurements):
-    sample_names = [os.path.splitext(sample_measurements[i])
-                   [0].replace("_", " ")
-                   for i in np.arange(len(sample_measurements))]
-    return sample_names
+
 
 def main():
     sample_measurements, _ = acquire_files()
