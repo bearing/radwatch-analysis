f6a8a4c57d5c53babc3c720e34394001924e13d5
Yaro Kaminskiy <yaroslavk@berkeley.edu>
2017-03-30 14:19:26 -0700

    Update Gamma_Reference.py

--- Gamma_Reference.py
+++ Gamma_Reference.py
@@ -1,59 +1,205 @@
-class base_reference_class(object):
-    """
-    Generates a reference object that contains a reference mass and
-    concentrations. Mass is in kg and concentrations are in percent for
-    K-40 and ppm for the rest. Conversions are currently unknown in their
-    origin and derivation.
-    """
-    def __init__(self, mass, ref_concentration, ref_concentration_error,
-                 conversion):
-        self.mass = mass
-        self.ref_concentration = ref_concentration
-        self.ref_concentration_error = ref_concentration_error
-        self.conversion = conversion
+import numpy as np
+import peakutils
+import pandas as pd
+import shutil as sh
+import fileinput
+import SPEFile
+import os
 
-class petri_reference_class(base_reference_class):
+energy_list = [351.93, 583.19, 609.31, 911.20, 1460.82, 1764.49,
+               2614.51]
+cal_headers = [351.93, 583.19, 609.31, 911.20, 1460.82, 1764.49,
+               2614.51, 'Output']
+
+def acquire_files():
     """
-    Uses the Petri dish sample data as the reference.
+    acquire_files gathers all the .Spe file in the current file directory and
+    returns a list containing all .Spe files.
     """
-    def spec_count_rate(self, mass, ref_concentration, ref_concentration_error,
-                 conversion, ref_specific_count_rate):
-        self.ref_spec_ct_rate = ref_specific_count_rate
+    sample_measurements = []
+    recal_names = []
+    dir_path = os.getcwd()
+    for filename in os.listdir(dir_path):
+        if filename.lower().endswith(".spe"):
+            # Ignore the background and reference spectra
+            if filename == "USS_Independence_Background.Spe":
+                pass
+            elif filename == "UCB018_Soil_Sample010_2.Spe":
+                pass
+            else:
+                if '_recal.Spe' in filename:
+                    recal_names.append(filename)
+                sample_measurements.append(filename)
 
-# Create a list of samples that uses an alternate reference.
-alt_ref_samples = ['UCB027']
-# Define the mass for the Petri soil reference sample.
-dirt_petri_mass = 1.18360
-petri_specific_count_rate = [3.11E-04, 2.15E-01, 9.35E-03, 2.17E-02, 1.05E-02,
-                             1.75E-02, 1.56E-02, 7.27E-02, 8.28E-03, 8.56E-03,
-                             4.11E-02, 5.25E-02, 3.10E-02, 1.07E-02, 1.50E-02]
+    sample_measurements.sort()
 
-# Define the mass for the S5F soil reference sample.
-dirt_S5F_mass = 1.19300
-dirt_k_40 = 2.57
-dirt_bi_214 = 1.97
-dirt_pb_214 = 1.97
-dirt_th_234 = 2.26
-dirt_tl_208 = 5.08
-dirt_ac_228 = 5.43
-dirt_pb_212 = 5.08
+    print("recal_names: ", recal_names, '\n\n')
 
-dirt_k_40_unc = 0.01
-dirt_bi_214_unc = 0.02
-dirt_pb_214_unc = 0.02
-dirt_th_234_unc = 0.07
-dirt_tl_208_unc = 0.05
-dirt_ac_228_unc = 0.07
-dirt_pb_212_unc = 0.05
-dirt_concentrations = [dirt_k_40, dirt_bi_214, dirt_pb_214, dirt_th_234,
-                       dirt_tl_208, dirt_ac_228, dirt_pb_212]
-dirt_concentrations_uncertainty = [dirt_k_40_unc, dirt_bi_214_unc,
-                                   dirt_pb_214_unc, dirt_th_234_unc,
-                                   dirt_tl_208_unc, dirt_ac_228_unc,
-                                   dirt_pb_212_unc]
-dirt_conversions = [309.6, 12.3, 4.07]
-soil_reference = base_reference_class(dirt_S5F_mass, dirt_concentrations,
-                           dirt_concentrations_uncertainty, dirt_conversions)
-petri_reference = petri_reference_class(dirt_petri_mass, dirt_concentrations,
-                           dirt_concentrations_uncertainty, dirt_conversions,
-                           petri_specific_activity)
+    for recal_name in recal_names:
+        recal_focus = recal_name.replace('_recal', '')
+
+        print(recal_focus)
+
+        if recal_focus in sample_measurements:
+            sample_measurements.remove(recal_focus)
+
+    sample_names = [os.path.splitext(sample_measurements[i])
+                    [0].replace("_", " ")
+                    for i in np.arange(len(sample_measurements))]
+
+    print("sample_names: ", sample_names, '\n\n')
+    print("sample_measurements: ", sample_measurements, '\n\n')
+
+    return sample_measurements, sample_names
+
+
+def calibration_check(spectrum):
+    '''
+    calibration_check will search for certain peaks that are expected to occur
+    in every measured spectra. The energies it searches for are based on peaks
+    that occur in background radiation. Once these peaks are found, it
+    compares the energy of that peak to the actual energy the peak should be
+    at. This check is based on expected detector resolution and if the energy
+    deviates to far from the expected value (if beyond half a FWHM), then
+    calibration_check sends a message indicating a fix is needed. Only Spectra
+    are taken as input.
+    '''
+    E0 = spectrum.energy_cal[0]
+    Eslope = spectrum.energy_cal[1]
+    energy_axis = E0 + Eslope*spectrum.channel
+
+    peak_channel = []
+    found_energy = []
+    offsets = []
+    skip = 0
+    fix = 0
+    for energy in energy_list:
+        # rough estimate of fwhm.
+        fwhm = 0.05*energy**0.5
+        energy_range = 0.015*energy
+
+        # peak gross area
+
+        start_region = np.flatnonzero(energy_axis > energy - energy_range)[0]
+
+        end_region = np.flatnonzero(energy_axis > energy + energy_range)[0]
+        y = spectrum.data[start_region:end_region]
+        indexes = peakutils.indexes(y, thres=0.5, min_dist=4)
+        tallest_peak = []
+        if indexes.size == 0:
+            print('peak not found')
+            offsets.append(np.nan)
+            skip += 1
+        else:
+            for i in range(indexes.size):
+                spot = spectrum.data[indexes[i]+start_region]
+                tallest_peak.append(spot)
+            indexes = indexes[np.argmax(tallest_peak)]
+            peak_channel.append(int(indexes+start_region))
+            found_energy.append(energy)
+            difference = abs((energy -
+                              float(energy_axis[int(indexes+start_region)])))
+            offsets.append(difference)
+            if difference > 0.5*fwhm:
+                fix += 1
+    if skip > 4:
+        message = 'error'
+    elif fix >= 4:
+        message = 'fix'
+    else:
+        message = 'fine'
+    offsets.append(message)
+    return(peak_channel, found_energy, message, offsets)
+
+
+def calibration_correction(measurement, channel, energy):
+    """
+    calibration_correction implements a corrected energy calibration based
+    on the array of channels and energies given as input. It performs a
+    least squares regression fit of the channels and energies and implements
+    the new energy calibration in a newly generated .Spe file. The new
+    spectra file will contain the same information with only the old
+    calibration changed.
+    """
+    cal_file = sh.copyfile(measurement, os.path.splitext(measurement)[0] +
+                           '_recal.Spe')
+    fix_measurement = SPEFile.SPEFile(cal_file)
+    fix_measurement.read()
+    old_cal = (str(float(fix_measurement.energy_cal[0])) + ' ' +
+               str(float(fix_measurement.energy_cal[1])))
+
+    a_matrix = np.vstack([channel, np.ones(len(channel))]).T
+    calibration_line = np.linalg.lstsq(a_matrix, energy)
+
+    e0 = float(calibration_line[0][1])
+    eslope = float(calibration_line[0][0])
+    new_cal = str(float(e0)) + ' ' + str(float(eslope))
+    with fileinput.FileInput(cal_file, inplace=1) as file:
+        for line in file:
+            print(line.replace(old_cal, new_cal).rstrip())
+    return(cal_file)
+
+
+def recalibrate(files):
+    cal_error = []
+    double_check = []
+    cal_offsets = []
+
+    for sample in files:
+        if '_recal.Spe' in sample.lower():
+            double_check.append(sample)
+            pass
+        else:
+            measurement = SPEFile.SPEFile(sample)
+            measurement.read()
+            [channel, energy, status, offsets] = calibration_check(measurement)
+            cal_offsets.append(offsets)
+            if status == 'fix':
+                print(('\nFixing calibration for %s \n' % sample))
+                cal_file = calibration_correction(sample, channel,
+                                                  energy)
+                double_check.append(cal_file)
+            elif status == 'error':
+                cal_error.append(sample)
+    for check in double_check:
+        Recal = SPEFile.SPEFile(check)
+        Recal.read()
+        status = calibration_check(Recal)[2]
+        if status == 'fix':
+            cal_error.append(check.replace('_recal.Spe', '.Spe'))
+    if cal_error == []:
+        pass
+    else:
+        with open('Error_Cal.txt', 'w') as file:
+            file.writelines('Check calibration in %s \n' % error for error in
+                            cal_error)
+    # for file in files:
+    #     if '_recal.Spe' in file:
+    #         files.remove(file)
+
+    files_updated = [os.path.splitext(files[i])
+                     [0].replace("_recal", "")
+                     for i in np.arange(len(files))]
+
+    calibration_table(files_updated, cal_headers, cal_offsets)
+
+
+def calibration_table(samples, headers, offsets):
+
+    cal_results = {}
+    for i in range(len(samples)):
+
+        cal_results[samples[i]] = np.array(offsets[i])
+
+    cal_frame = pd.DataFrame(cal_results, index=headers)
+    cal_frame = cal_frame.T
+    cal_frame.index.name = 'Filename'
+    cal_frame.to_csv('calibration_results.csv')
+
+
+def main():
+    sample_measurements, _ = acquire_files()
+    recalibrate(sample_measurements)
+
+if __name__ == '__main__':
+    main()
