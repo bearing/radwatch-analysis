92ca8590823e3a7d3b5f2d811e0538f60f3a3b9e
alihanks <j.ali.hanks@gmail.com>
2020-02-21 13:12:11 -0800

    updates to peak counts methods and examples

--- PF.py
+++ PF.py
@@ -14,21 +14,32 @@ def f_near(energy_array,energy): #finds index of closest energy in spectrum to t
     return idx
 
 class PF(object):
-    def __init__(self,source_energies,source_activities,spectrum,background,branching_ratio):
+    def __init__(self,spectrum, background, source_energies, 
+                      source_activities = None, source_isotopes = None, branching_ratio = None):
+        self.spectrum = Spectrum.from_file(spectrum)
+        self.background = Spectrum.from_file(background)
         self.source_energies = source_energies
+
         self.source_activities = source_activities
-        self.spectrum = spectrum
-        self.background = background
+        self.source_isotopes = source_isotopes
         self.branching_ratio = branching_ratio
 
+        self.integrals = []
+
+    def calibration(integrals): #calculate efficiencies
+        efficiencies = []
+        for x in range(0,len(self.source_activities)):
+            iso = bq.tools.Isotope(self.source_isotopes[x])
+            efficiency = self.integrals[x]/(self.source_activities[x]*self.branching_ratio[x])
+            efficiencies = np.append(efficiencies,efficiency)
+        return efficiencies
 
 
     def get_counts(self):
-        spec = Spectrum.from_file(self.spectrum) #import spectrum data
-        spec_counts = spec.counts_vals #spectrum counts
-        bg = Spectrum.from_file(self.background) #import spectrum data
-        bg_counts = bg.counts_vals #background counts
-        sub_spec = spec - bg #background subtraction
+        spec_counts = self.spectrum.counts_vals #spectrum counts
+        bg_counts = self.background.counts_vals #background counts
+
+        sub_spec = self.spectrum - self.background #background subtraction
         spec_energies = sub_spec.energies_kev #all energues
         spec_counts = spec_counts - bg_counts #all counts
         integrals = []
@@ -42,19 +53,13 @@ class PF(object):
             mu = fit.result.params['gauss_mu'].value
             sigma =fit.result.params['gauss_sigma'].value
             def gaussian(x):
-                return (spec.livetime * amp /(m.sqrt(2*m.pi)*sigma)) * m.exp(- ((x-mu)**2) / (2*sigma**2))
+                return (self.spectrum.livetime * amp /(m.sqrt(2*m.pi)*sigma)) * m.exp(- ((x-mu)**2) / (2*sigma**2))
             integral = integrate.quad(gaussian, idx-100, idx+100)
             integrals = np.append(integrals,integral[0])
+        self.integrals = integrals
         return integrals
 
     def Efficiency(self):
-        def calibration(integrals): #calculate efficiencies
-            efficiencies = []
-            for x in range(0,len(self.source_activities)):
-                iso = bq.tools.Isotope(self.source_isotopes[x])
-                efficiency = integrals[x]/(self.source_activities[x]*branching_ratio[x])
-                efficiencies = np.append(efficiencies,efficiency)
-            return efficiencies
         spec = Spectrum.from_file(self.spectrum) #import spectrum data
         spec_counts = spec.counts_vals #spectrum counts
         bg = Spectrum.from_file(self.background) #import spectrum data
