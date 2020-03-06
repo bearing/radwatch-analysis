87bce6d0ba68d2000d4364e2acb4d5489556ab0f
Daniel <shmandeltoid@gmail.com>
2020-03-05 17:47:11 -0800

    updates

--- naa_isotope_analyzer.py
+++ naa_isotope_analyzer.py
@@ -43,6 +43,7 @@ def naa_isotope_analyzer(energies):
         background_isotopes_energy.append(naa_background.background(energies[i])['identified_isotopes_energy'])
         background_isotopes_br.append(naa_background.background(energies[i])['identified_isotopes_br'])
 
+    print("Backround isotopes", background_isotopes)
     #checks to see if any of the peaks are due to single escape peaks,
     #double escape peaks, and sum peaks.
     peak_effects_info = naa_peak_effects.peak_effects(energies)
@@ -69,21 +70,39 @@ def naa_isotope_analyzer(energies):
     #associated with every energy from the energy list.
     nndc_info = []
     for i in range(len(energies)):
-         nndc_info.append(nndc.fetch_decay_radiation(t_range=[0, None], i_range=(1, None), type='Gamma', e_range=[energies[i]-1, energies[i]+1]))
+         nndc_info.append(nndc.fetch_decay_radiation(t_range=[0, None], i_range=(1, None), type='Gamma', e_range=[energies[i]-2, energies[i]+10]))
 
     #checks to see if the 'parents' of the isotopes returned by the Becquerel
     #module are naturally occuring isotopes.
-    nndc_info_verified_isotope = naa_isotope_verifier.isotope_verifier(nndc_info)['nndc_info_verified_isotope']
-    nndc_info_verified_energy = naa_isotope_verifier.isotope_verifier(nndc_info)['nndc_info_verified_energy']
-    nndc_info_verified_br = naa_isotope_verifier.isotope_verifier(nndc_info)['nndc_info_verified_br']
+    all_nndc_info_verified_isotope = naa_isotope_verifier.isotope_verifier(nndc_info)['nndc_info_verified_isotope']
+    all_nndc_info_verified_energy = naa_isotope_verifier.isotope_verifier(nndc_info)['nndc_info_verified_energy']
+    all_nndc_info_verified_br = naa_isotope_verifier.isotope_verifier(nndc_info)['nndc_info_verified_br']
+    print("Possible isotopes", all_nndc_info_verified_isotope)
+    print("Possible isotope br", all_nndc_info_verified_br)
 
+    nndc_info_verified_isotope =[]
+    nndc_info_verified_energy = []
+    nndc_info_verified_br =[]
+
+    #removes isotopes with low branching ratios before tallying how many times it appears in the verified list of isotopes
+    for i in range(len(all_nndc_info_verified_isotope)):
+        nndc_info_verified_isotope.append([])
+        nndc_info_verified_energy.append([])
+        nndc_info_verified_br.append([])
+        for j in range(len(all_nndc_info_verified_isotope[i])):
+            if all_nndc_info_verified_br[i][j]>5.0:
+                nndc_info_verified_isotope[i].append(all_nndc_info_verified_isotope[i][j])
+                nndc_info_verified_energy[i].append(all_nndc_info_verified_energy[i][j])
+                nndc_info_verified_br[i].append(all_nndc_info_verified_br[i][j])
+
+    print("Cut isotopes ", nndc_info_verified_isotope)
     #counts how many times an isotope is repeated in nndc_info_verified_isotope.
     #This is done for instances when multiple isotopes emit the same energy
     #photon and only one isotope can be chosen to represent a given energy.
     unpacked_isotopes = list(chain.from_iterable(nndc_info_verified_isotope))
     tally = [ (i,unpacked_isotopes.count(i)) for i in set(unpacked_isotopes) ]
     tally.sort(key=itemgetter(1), reverse=True)
-    
+
     #chooses most probable isotope for energies in which multiple isotopes emit
     #at the same energy. Criteria is based on the total amount of times an
     #isotope appears as a possible candidate for all peak energies.
@@ -104,6 +123,7 @@ def naa_isotope_analyzer(energies):
                 pass
         except:
             pass
+    print("Verified Isotopes,", nndc_info_verified_isotope)
 
     #for every energy and branching ratio that contains an uncertainty, the
     #below code will only extract the nominal value and discard the standard
@@ -118,7 +138,7 @@ def naa_isotope_analyzer(energies):
             pass
 
     #since the branching ratios returned from Becquerel are not normalized to 1,
-    #the below code will divide each branching ratio returned from Becquerel by 
+    #the below code will divide each branching ratio returned from Becquerel by
     #100 in order to keep all the branching ratios consistent.
 
     for i in range(len(nndc_info_verified_isotope)):
@@ -165,7 +185,7 @@ def naa_isotope_analyzer(energies):
     """
 
     #Formats the data to output to a csv file in which the isotopes will be
-    #listed in descending order (along with their relevant peak information) 
+    #listed in descending order (along with their relevant peak information)
     #based on the number of peak energies detected.
     unpacked_isotopes = list(chain.from_iterable(isotopes))
     tally = [ (i,unpacked_isotopes.count(i)) for i in set(unpacked_isotopes) ]
@@ -184,14 +204,14 @@ def naa_isotope_analyzer(energies):
         temp_br = []
  #       temp_ordered_net_area = []
  #       temp_ordered_net_area_unc = []
- #       temp_ordered_peak_cps = []        
+ #       temp_ordered_peak_cps = []
  #       temp_ordered_fwhm = []
         for j in range(len(isotopes)):
             if tally[i][0] in isotopes[j]:
                 temp_energies.append(energies[j])
  #               temp_ordered_net_area.append(net_area[j])
  #               temp_ordered_net_area_unc.append(net_area_unc[j])
- #               temp_ordered_peak_cps.append(peak_cps[j])                
+ #               temp_ordered_peak_cps.append(peak_cps[j])
  #               temp_ordered_fwhm.append(fwhm[j])
 
                 try:
@@ -204,7 +224,7 @@ def naa_isotope_analyzer(energies):
         ordered_br.append(temp_br)
 #        ordered_net_area.append(temp_ordered_net_area)
 #        ordered_net_area_unc.append(temp_ordered_net_area_unc)
-#        ordered_peak_cps.append(temp_ordered_peak_cps)        
+#        ordered_peak_cps.append(temp_ordered_peak_cps)
 #        ordered_fwhm.append(temp_ordered_fwhm)
 
 #    results2 = {'isotopes':ordered_isotopes,'energies':ordered_energies,
